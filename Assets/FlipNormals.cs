6adbe16e0bed4fde620ccf3d7726c5c694031fab
bfrager <btfrager@gmail.com>
2016-02-10 00:23:39 -0800

    Basic Root Level Prototype no data

---
+++ Assets/FlipNormals.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using UnityEngine;
+[ExecuteInEditMode]
+public class FlipNormals : MonoBehaviour
+{
+	void Start()
+	{
+        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
+	}
+}
